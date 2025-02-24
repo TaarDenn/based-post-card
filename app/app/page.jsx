04f3801c6f2a6a95b07f4aeb2f5c58f7e39d5046
TaarDenn <taar.denn@gmail.com>
2025-02-25 03:00:21 +0330

    add frame manifest

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,10 +1,34 @@
 import MainApp from "@/components/main-app";
-import { METADATA } from "@/constants/metadata";
 
-export const revalidate = 300;
+const appUrl = process.env.NEXT_PUBLIC_URL;
+const ogImageUrl = `${appUrl}/open-graph/og.png`;
 
 export async function generateMetadata() {
-  return METADATA;
+  return {
+    title: "Post-Card.fun",
+    openGraph: {
+      title: "Post-Card.fun",
+      description: "Post handcrafted NFTs to frens!",
+      type: "website",
+      siteName: "Post-card.fun",
+      url: appUrl,
+      images: [
+        {
+          url: ogImageUrl,
+          width: 1200,
+          height: 630,
+          alt: "post-card.fun og",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Post-Card.fun",
+      description: "Post handcrafted NFTs to frens!",
+      creator: "@taardenn",
+      images: [ogImageUrl],
+    },
+  };
 }
 
 export default function Home() {
--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,48 @@ import Frame from "@/components/frame";
 
 export const revalidate = 300;
 
-import { METADATA, FRAMEDATA } from "@/constants/metadata";
+const frame = {
+  version: "next",
+  imageUrl: ogImageUrl,
+  button: {
+    title: "Launch Post-card.fun",
+    action: {
+      type: "launch_frame",
+      name: "POST your handcrafted NFT to a fren!",
+      url: appUrl,
+      splashImageUrl: `${appUrl}/splash/splash.png`,
+      splashBackgroundColor: "#0052ff",
+    },
+  },
+};
 
 export async function generateMetadata() {
   return {
-    ...METADATA,
+    title: "Post-Card.fun",
+    openGraph: {
+      title: "Post-Card.fun",
+      description: "Post handcrafted NFTs to frens!",
+      type: "website",
+      siteName: "Post-card.fun",
+      url: appUrl,
+      images: [
+        {
+          url: ogImageUrl,
+          width: 1200,
+          height: 630,
+          alt: "post-card.fun og",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Post-Card.fun",
+      description: "Post handcrafted NFTs to frens!",
+      creator: "@taardenn",
+      images: [ogImageUrl], // Must be an absolute URL
+    },
     other: {
-      "fc:frame": JSON.stringify(FRAMEDATA),
+      "fc:frame": JSON.stringify(frame),
     },
   };
 }
