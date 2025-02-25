77f3a6abb0a3ae5eb22e28869b21a0047ca8a9e5
TaarDenn <taar.denn@gmail.com>
2025-02-25 07:17:17 +0330

    why farcaster? why?

--- providers/lazy-wagmi-provider.jsx
+++ providers/lazy-wagmi-provider.jsx
@@ -1,11 +1,12 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const LazyWagmi = dynamic(() => import("@/providers/wagmi-provider"), {
-  ssr: false,
-});
+// import dynamic from "next/dynamic";
+import { WagmiProvider } from "@/providers/wagmi-provider";
+// const LazyWagmi = dynamic(() => import("@/providers/wagmi-provider"), {
+//   ssr: false,
+// });
 
 export default function Provider({ children }) {
-  return <LazyWagmi>{children}</LazyWagmi>;
+  //   return <LazyWagmi>{children}</LazyWagmi>;
+  return <WagmiProvider>{children}</WagmiProvider>;
 }
